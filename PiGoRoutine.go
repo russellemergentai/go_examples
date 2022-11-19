fedf0a11b9e739979cfe0b7c7b6f3dc025a10e55
Russell <52071229+russellemergentai@users.noreply.github.com>
2022-11-19 15:57:36 +0000

    Create PiGoRoutine.go

---
+++ PiGoRoutine.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func calculate(ch chan float64) {
+	acc := 0
+	n := 10000000
+
+	for i := 0; i < n; i++ {
+		var x = rand.Float64()
+		var y = rand.Float64()
+		xx := math.Pow(x, 2)
+		yy := math.Pow(y, 2)
+		r := (xx + yy)
+		if r < 1.0 {
+			acc++
+		}
+	}
+
+	output := float64(acc) * 4 / float64(n)
+	ch <- output
+}
+
+func main() {
+	ch1 := make(chan float64)
+	go calculate(ch1)
+	val := <-ch1
+	fmt.Println(val)
+}
